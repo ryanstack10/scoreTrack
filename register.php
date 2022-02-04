394a908f8ceb2a3e56ce8b1e1cafe88419ed6dba
stackr10 <ryanstack@outlook.com>
2022-02-04 03:05:26 -0500

    testing logic

--- register.php
+++ register.php
@@ -26,7 +26,7 @@
       $sql = "INSERT INTO user (fname, lname, username, team_id) VALUES ('$fname', '$lname', '$username', (SELECT team_id FROM team WHERE teamname = '$team'));";
       $result = mysqli_query($db,$sql);
 	  
-	  echo "<script>alert($result)</script>";
+	  var_dump($result);
       
 		  $sql = "SELECT SUBSTRING(SHA2(RAND(), 512), -32) AS salt;";
 		  $result = mysqli_query($db,$sql);
@@ -37,17 +37,17 @@
 		  $sql = "INSERT INTO security (user_id, password, salt) VALUES ((SELECT user_id FROM user WHERE username='$username'), SHA2(CONCAT('$password', '$salt'), 512),'$salt}');";
 		  $result = mysqli_query($db,$sql);
 		  
-		  if(!$result){
-			  $sql = "DELETE FROM user WHERE username='$username';";
-			  $result = mysqli_query($db,$sql);
-			  $_SESSION['registration'] = "failed";
-
-	          header("location: index.php");
-		  } else {
-			  $_SESSION['registration'] = "successful";
-
-	          header("location: index.php");
-		  }
+		  // if(!$result){
+// 			  $sql = "DELETE FROM user WHERE username='$username';";
+// 			  $result = mysqli_query($db,$sql);
+// 			  $_SESSION['registration'] = "failed";
+//
+// 	          header("location: index.php");
+// 		  } else {
+// 			  $_SESSION['registration'] = "successful";
+//
+// 	          header("location: index.php");
+// 		  }
 
   	}
    }
