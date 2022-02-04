0351869882c66c7f9789fa2e1ef00e7909bb5b3b
stackr10 <ryanstack@outlook.com>
2022-02-04 05:22:50 -0500

    added home page nav and footer

--- home.php
+++ home.php
@@ -6,12 +6,53 @@
 		header("location: index.php");
 	}
 ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-	<head>
+<head>
+	<meta charset="UTF-8">
+	<title>scoreTrack</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="header">
+		<div class="section">
+			<div class="logo">
+				<a href="index.html">ScoreTrack</a>
+			</div>
+			<ul>
+				<li class="selected">
+					<a href="index.html">home</a>
+				</li>
+				<li>
+					<a href="about.html">about</a>
+				</li>
+				<li>
+					<a href="hairstyle.html">hairstyles</a>
+				</li>
+				<li>
+					<a href="news.html">news</a>
+				</li>
+				<li>
+					<a href="contact.html">contact</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="body">
 		
-	</head>
-	<body>
-		<p>home page</p>
-	</body>
+	</div>
+	<div id="footer">
+		<div>
+			<div class="connect">
+				<a href="http://freewebsitetemplates.com/go/twitter/" id="twitter">twitter</a>
+				<a href="http://freewebsitetemplates.com/go/facebook/" id="facebook">facebook</a>
+				<a href="http://freewebsitetemplates.com/go/googleplus/" id="googleplus">googleplus</a>
+				<a href="http://pinterest.com/fwtemplates/" id="pinterest">pinterest</a>
+			</div>
+			<p>
+				&copy; copyright 2023 | all rights reserved.
+			</p>
+		</div>
+	</div>
+</body>
 </html>
